accf59db6612c88664f1999f7e1a612dcd9af03e
liuxin7 <l13611219980@163.com>
2017-08-21 18:12:36 +0800

    添加项目文件。

---
+++ UploadFile/Controllers/FileController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UploadFile.Controllers
+{
+    public class FileController : Controller
+    {
+        // GET: File
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SaveFile()
+        {
+            //   用表单action 上传 文件    没有提交文件 cout也是1     有问题   所以在下面在 判断一层   获取的名称时候 为空
+            if (Request.Files.Count > 0)
+            {
+                if (string.IsNullOrEmpty(Request.Files[0].FileName))
+                {
+                    Request.Files[0].SaveAs(Server.MapPath("~/App_Data/") + Request.Files[0].FileName);
+                    return RedirectToAction("UploadFile", Content("保存成功"));
+                }
+                Math.Min(1, 1);
+            }
+            return Content("<script >alert('请选择 文件');</script >", "text/html");
+
+        }
+
+        /// <summary>
+        /// HTML5  fromData  上传 文件
+        /// </summary>
+        /// <returns> 返回 结果</returns>
+        public string SaveUpload()
+        {
+            string str = string.Empty;
+            if (Request.Files.Count > 0)  // 这个  是不是 
+            {
+                Request.Files[0].SaveAs(Server.MapPath("~/UploadFile/") + Path.GetFileName(Request.Files[0].FileName));
+                str = "保存成功";
+            }
+            else
+                str = "没有读到文件";
+
+            return str;
+        }
+
+        /// <summary>
+        ///  上传大文件的时候     需要一点一点的 上传  要不上传不了
+        /// </summary>
+        /// <returns></returns>
+        public string BlockUpload()
+        {
+            string str = string.Empty;
+            try
+            {
+                // 
+                var filePath = Server.MapPath("~/UploadFile/") + Request.Form["fileName"];
+                //创建一个追加（FileMode.Append）方式的文件流
+                using (FileStream fs = new FileStream(filePath, FileMode.Append, FileAccess.Write))
+                {
+                    using (BinaryWriter bw = new BinaryWriter(fs))
+                    {
+                        //读取文件流
+                        BinaryReader br = new BinaryReader(Request.Files[0].InputStream);
+                        //将文件留转成字节数组
+                        byte[] bytes = br.ReadBytes((int)Request.Files[0].InputStream.Length);
+                        //将字节数组追加到文件
+                        bw.Write(bytes);
+                    }
+                }
+                str = "保存成功";
+            }
+            catch (Exception ex)
+            {
+                str = ex.Message.ToString();
+            }
+            return str;
+        }
+
+        /// <summary>
+        ///  文件 拖拽
+        /// </summary>
+        /// <returns></returns>
+        public string FileOnDrop()
+        {
+            string str = string.Empty;
+            if (Request.Files.Count > 0)  // 这个  是不是 
+            {
+
+                Request.Files[0].SaveAs(Server.MapPath("~/UploadFile/") + Path.GetFileName(Request.Files[0].FileName));
+                str = "保存成功";
+            }
+            else
+                str = "没有读到文件";
+            return str;
+        }
+
+        /// <summary>
+        ///  文件 粘贴
+        /// </summary>
+        /// <returns></returns>
+        public string FileOnPaste()
+        {
+
+            //  var filePath = Server.MapPath("~/App_Data/") + Request.Form["fileName"];     使用这个 方法的时候   
+            // 在前台的js  fromData 中    添加  formData.append('fileName', "自定义的名称")  就可以直接获取到这个的名称
+            string str = string.Empty;
+            //string file = Server.MapPath("~/UploadFile/") + Path.GetFileName(Request.Files[0].FileName);
+            string file = Server.MapPath("~/UploadFile/") + Request.Form["fileName"];
+            if (Request.Files.Count > 0)
+            {
+                Request.Files[0].SaveAs(file);
+                str = "保存成功";
+            }
+            else
+                str = "没有读到文件";
+            return str;
+        }
+
+    }
+
+
+}
---
+++ UploadFile/Scripts/file/file.js
@@ -0,0 +1,46 @@
+﻿//   元素拖动 时触发  事件 ondrop
+$(".divFile7")[0].ondrop = function (event) {
+    event.preventDefault();//   不要执行与事件关联的默认动作  防止浏览器的默认处理数据
+    var files = event.dataTransfer.files;//获取拖上来的文件
+    $(".divFile7").html(files[0].name)
+    var formData = new FormData(); //  初始化 对象
+    //for (var i = 0; i < files.length; i++) {   //  如果上传多个    循环 取到的 file 文件    依次  上传
+    //    files[i]
+    //}
+    formData.append("files", files[0]);//将文件塞入FormData
+
+    $.ajax({
+        url: "/File/FileOnDrop",
+        type: "POST",
+        data: formData,
+        processData: false,  // 告诉jQuery不要去处理发送的数据
+        contentType: false,   // 告诉jQuery不要去设置Content-Type请求头
+        success: function (responseText) {
+            alert(responseText);
+
+        }
+    });
+};
+
+//   粘贴文件时触发 事件 onpaste    
+$(".divFile7")[0].onpaste = function (event) {
+    event.preventDefault();//不要执行与事件关联的默认动作
+    var clipboard = event.clipboardData.items[0];//剪贴板数据
+    if (clipboard.kind == 'file' || clipboard.type.indexOf('image') > -1) {//判断是图片格式
+        var imageFile = clipboard.getAsFile();//获取文件
+
+        var formData = new FormData();
+        formData.append('files', imageFile);
+        formData.append('fileName', imageFile.name);//这里给文件命个名（或者直接在后台保存的时候命名）
+        $.ajax({
+            url: "/File/FileOnPaste",
+            type: "POST",
+            data: formData,
+            processData: false,  // 告诉jQuery不要去处理发送的数据
+            contentType: false,   // 告诉jQuery不要去设置Content-Type请求头
+            success: function (responseText) {
+                alert(responseText);
+            }
+        });
+    }
+};
